8be5f4a1b921606d54aeb0277e20b89c6b2f27b3
Jessica Woods <48572600+JessicaWoods03@users.noreply.github.com>
2020-01-05 18:33:48 -0500

    Add files via upload

--- Light sensor test.py
+++ Light sensor test.py
@@ -3,9 +3,17 @@ import busio
 import board
 import adafruit_veml6070 
 
+# Make sure your i2c is configured on your raspberry pi for your sensor boards
+# if not follow this website https://learn.adafruit.com/
+# adafruits-raspberry-pi-lesson-4-gpio-setup/configuring-i2c
+
 with busio.I2C(board.SCL, board.SDA) as i2c:
     uv = adafruit_veml6070.VEML6070(i2c)
+    
+    #this tells you available commands for the sensor
     print(dir(uv))
+    
+    #on the pdf and tutorial uv.read is unavaible, so tried this and it works
     uv_raw = uv.uv_raw
     risk_level = uv.get_index(uv_raw)
 
